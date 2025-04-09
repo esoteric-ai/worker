c214366ace0c806fdd210b5b8ac8eb2a719e753f
anunknowperson <kiselev.sereja@gmail.com>
2025-04-09 05:18:25 +0300

    fix: mm

--- backends/vllm.py
+++ backends/vllm.py
@@ -206,8 +206,9 @@ class VllmBackend(Backend):
             "frequency_penalty": params.get("frequency_penalty", 0.0),
             "presence_penalty": params.get("presence_penalty", 0.0)
         }
-        print("mm_orice", mm_processor_kwargs)
-        if mm_processor_kwargs != {}:
+        
+        if len(mm_processor_kwargs) != 0:
+            print("mm_orice", mm_processor_kwargs)
             extra_body["mm_processor_kwargs"] = mm_processor_kwargs
         
         try:
